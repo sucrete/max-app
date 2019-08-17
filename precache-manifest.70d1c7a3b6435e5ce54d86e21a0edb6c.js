a2b31580264b4536acd9b05468b2d6d795a1ffb2
William Andree <williamandree@gmail.com>
2019-08-17 13:03:03 -0500

    Updates

---
+++ precache-manifest.70d1c7a3b6435e5ce54d86e21a0edb6c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ac176ee57c5a3efe0ceb8ce535c35af",
+    "url": "/max-app/index.html"
+  },
+  {
+    "revision": "f4811b191bdfd1940caa",
+    "url": "/max-app/static/css/main.20102bc9.chunk.css"
+  },
+  {
+    "revision": "9f0f47cc047dd2ddec08",
+    "url": "/max-app/static/js/2.3d3e5e3a.chunk.js"
+  },
+  {
+    "revision": "f4811b191bdfd1940caa",
+    "url": "/max-app/static/js/main.831a8398.chunk.js"
+  },
+  {
+    "revision": "1f21619e51f74b7cc75e",
+    "url": "/max-app/static/js/runtime~main.37789ba3.js"
+  }
+]);
